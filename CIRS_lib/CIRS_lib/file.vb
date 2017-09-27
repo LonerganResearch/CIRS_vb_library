a21fc0c4fa16af7e9872d775c5b1c60dbc1e3d9e
Rei Kaneko <rei.kaneko17@gmail.com>
2017-09-27 17:29:02 +1000

    First commit. Might be worth changing some names around to be a bit more meaningful. writeToFile works fine.

---
+++ CIRS_lib/CIRS_lib/file.vb
@@ -0,0 +1,18 @@
+﻿Public Class file
+    Public Sub writeToFile(ByVal input As String, ByVal fileName As String)
+        If IO.File.Exists(fileName) = False Then
+            IO.File.Create(fileName).Dispose()
+        Else
+            If MsgBox("File already exists. Overwrite?", MsgBoxStyle.YesNo + MsgBoxStyle.SystemModal, "Overwrite file?") = MsgBoxResult.Yes Then
+                IO.File.Delete(fileName)
+            End If
+        End If
+        Try
+            Dim objWriter As New IO.StreamWriter(fileName, True)
+            objWriter.WriteLine(input)
+            objWriter.Close()
+        Catch ex As Exception
+            MsgBox("Please close the file first.", MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+End Class
