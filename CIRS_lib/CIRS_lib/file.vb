7efe524f9a51458b841ae3b5797f693a5dfb33ef
Rei Kaneko <rei.kaneko17@gmail.com>
2017-10-09 10:44:12 +1100

    Made changes in merge comment.

--- CIRS_lib/CIRS_lib/file.vb
+++ CIRS_lib/CIRS_lib/file.vb
@@ -1,6 +1,6 @@
 ﻿Public Class file
     Public Sub write(ByVal input As String, ByVal fileName As String, Optional ByVal append As Boolean = True) 'Write text to a file
-        If IO.File.Exists(fileName) = True And append = False Then
+        If IO.File.Exists(fileName) = True Then
             If MsgBox(fileName & " already exists. Are you sure you want to overwrite it?", MsgBoxStyle.YesNo + MsgBoxStyle.SystemModal, "Overwrite file?") = MsgBoxResult.No Then
                 append = False
             End If
@@ -14,11 +14,11 @@
         End Try
     End Sub
 
-    Public Function textBetweenStrings(input As String, startString As String, endString As String) As String 'Find a string within a string using start and stop strings/characters
+    Public Function parseInString(input As String, startString As String, endChar As Char) 'Find a string within a string using start and stop strings/characters
         Dim x As Integer = (input.IndexOf(startString) + Len(startString))
         Dim output As String = ""
         Try
-            While input(x) <> endString(0)
+            While input(x) <> endChar
                 output += input(x)
                 x += 1
             End While
